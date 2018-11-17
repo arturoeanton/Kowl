7ab577a5b7d668de7c2cda04f93077cef0caee7a
Arturo Elias Anton <arturoeliasanton@Arturos-MacBook-Pro.local>
2018-11-17 11:59:22 -0300

    parser and add ticker function and kNow

--- example.js
+++ example.js
@@ -1,49 +1,33 @@
 function not_found(name, op, args) {
-  
-    console.log(name + " - " + op + " - " + args)
+    console.log(name + " - " + op + " - " + args, kNow())
+}
 
+function ticker(name, op, args, t) {
+    console.log(name + " - " + op + " - " + args, kNow())
 }
 
 function exist(name, op, args) {
-    data = kFileToString(name) ;
-    console.log(data)
-    console.log(name + " - " + op + " - " + args)
+    console.log(name + " - " + op + " - " + args, kNow())
 }
 
 function write(name, op, args) {
-    if (kGetEnv("kFlag") != 1){
-        data = kFileToString(name) ;
-        kStringToFile("8080",name)
-        kSetEnv("kFlag",1)
-    }else{
-        kSetEnv("kFlag",0)
-    }
+    console.log(name + " - " + op + " - " + args, kNow())
+
 }
 
 function create(name, op, args) {
-    console.log(name + " - " + op + " - " + args)
+    console.log(name + " - " + op + " - " + args, kNow())
 }
 
 function remove(name, op, args) {
-    console.log(name + " - " + op + " - " + args)
+    console.log(name + " - " + op + " - " + args, kNow())
 }
 
 function rename(name, op, args) {
-    data = kFileToString(name) ;
-    console.log(data[0].split("\n")[1])
-    console.log(name + " - " + op + " - " + args)
+    console.log(name + " - " + op + " - " + args, kNow())
+
 }
 
 function chmod(name, op, args) {
-   /* data =  kEncrypt("123", "Hello word! ")
-    console.log(data[0])
-    data =   kDecrypt("123", data[0])
-    console.log(data[0])
-
-
-
-
-    data = kFileToString(name) 
-    console.log(data[0].split("\n")[1])*/
-    console.log(name + " - " + op + " - " + args)
+    console.log(name + " - " + op + " - " + args, kNow())
 }
