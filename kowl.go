4aba079089ac803d16d13366b2057e2694fd99a6
Arturo Elias Anton <arturoeliasanton@Arturos-MacBook-Pro.local>
2018-11-17 11:59:22 -0300

    parser and add ticker function and kNow

--- kowl.go
+++ kowl.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"Kowl/js"
+	"fmt"
 	"github.com/fsnotify/fsnotify"
+	"github.com/jessevdk/go-flags"
 	"github.com/robertkrimen/otto"
 	"github.com/robertkrimen/otto/underscore"
 	"gopkg.in/h2non/gentleman.v2"
@@ -14,28 +16,26 @@ import (
 	"time"
 )
 
-func main() {
-
-	if len(os.Args) != 3 {
-		log.Fatalln("kowl [filename]  [script]")
+var (
+	opts struct {
+		Filename       string `short:"f" required:"true" long:"filename" description:"filename that wants to be observed"`
+		Script         string `short:"j" required:"true" long:"javascript" description:"Js that wants that executes the actions"`
+		Millisecond    *int   `short:"m" default:"1000" long:"millisecond" description:"Millisecond change check defaul 1000"`
+		FlagNotWatcher bool   `short:"w" long:"flagNotWatcher" description:"Watcher disable"`
 	}
+	errCh chan error
+)
 
-	filename := os.Args[1]
-	script := os.Args[2]
-
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer watcher.Close()
-
-	waitAndAddWatcher(script, filename, watcher)
-
-	renameCh := make(chan bool)
-	removeCh := make(chan bool)
-	errCh := make(chan error)
-
-	go func() {
+func observer(watcher *fsnotify.Watcher, script, filename string) func() {
+	runjs(script, "EXIST", filename)
+	return func() {
+		var err error
+		watcher, err = fsnotify.NewWatcher()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer watcher.Close()
+		watcher.Add(filename)
 		for {
 			select {
 			case event := <-watcher.Events:
@@ -46,10 +46,8 @@ func main() {
 					runjs(script, "CREATE", event.Name)
 				case event.Op&fsnotify.Remove == fsnotify.Remove:
 					runjs(script, "REMOVE", event.Name)
-					removeCh <- true
 				case event.Op&fsnotify.Rename == fsnotify.Rename:
 					runjs(script, "RENAME", event.Name)
-					renameCh <- true
 				case event.Op&fsnotify.Chmod == fsnotify.Chmod:
 					runjs(script, "CHMOD", event.Name)
 				}
@@ -57,53 +55,76 @@ func main() {
 				errCh <- err
 			}
 		}
-	}()
+	}
+}
 
-	go func() {
-		for {
-			select {
-			case <-renameCh:
-				waitAndAddWatcher(script, filename, watcher)
-			case <-removeCh:
-				waitAndAddWatcher(script, filename, watcher)
+func main() {
+	_, err := flags.ParseArgs(&opts, os.Args)
+	if err != nil {
+		os.Exit(-1)
+	}
+	if err != nil {
+		os.Exit(-1)
+	}
+	filename := opts.Filename
+	script := opts.Script
+	millisecond := *opts.Millisecond
+	flagNotWatcher := opts.FlagNotWatcher
+
+	fmt.Println(filename)
+	fmt.Println(script)
+	fmt.Println(millisecond)
+	fmt.Println(flagNotWatcher)
+	errCh = make(chan error)
+
+	var watcher fsnotify.Watcher
+	var flagExecutedObserver bool
+
+	if !flagNotWatcher {
+		flagExecutedObserver = false
+		ticker := time.NewTicker(time.Duration(1) * time.Second)
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					{
+						if _, err := os.Stat(filename); os.IsNotExist(err) {
+							flagExecutedObserver = false
+						} else {
+							if !flagNotWatcher {
+								if !flagExecutedObserver {
+									flagExecutedObserver = true
+									go observer(&watcher, script, filename)()
+								}
+							}
+						}
+					}
+				}
 			}
-		}
-	}()
+		}()
+	}
 
+	if millisecond > 0 {
+		ticker := time.NewTicker(time.Duration(millisecond) * time.Millisecond)
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					{
+						if _, err := os.Stat(filename); os.IsNotExist(err) {
+							runjs(script, "NOT_FOUND", filename)
+						} else {
+							if (*opts.Millisecond) > 0 {
+								runjs(script, "TICKER", filename)
+							}
+						}
+					}
+				}
+			}
+		}()
+	}
 	log.Fatalln(<-errCh)
-}
 
-func waitUntilFind(script, filename string) error {
-	b := false
-	for {
-		time.Sleep(1 * time.Second)
-		_, err := os.Stat(filename)
-		if err != nil {
-			if os.IsNotExist(err) {
-				b = true;
-				runjs(script, "NOT_FOUND", filename)
-				continue
-			} else {
-				return err
-			}
-		}
-		if b {
-			runjs(script, "EXIST", filename)
-		}
-		break
-	}
-	return nil
-}
-
-func waitAndAddWatcher(script, filename string, watcher *fsnotify.Watcher) {
-	err := waitUntilFind(script, filename)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	err = watcher.Add(filename)
-	if err != nil {
-		log.Fatalln(err)
-	}
 }
 
 func runjs(script string, op string, name string) {
@@ -132,19 +153,17 @@ func runjs(script string, op string, name string) {
 	vm.Set("kBodyXML", body.XML)
 	vm.Set("kBodyString", body.String)
 
-	vm.Set("kGetEnv",os.Getenv)
-	vm.Set("kSetEnv",os.Setenv)
-	vm.Set("kHostname",os.Hostname)
-	vm.Set("kGetpid",os.Getpid)
-	vm.Set("kGetppid",os.Getppid)
-	vm.Set("kGetgid",os.Getgid)
-	vm.Set("kGetuid",os.Getuid)
-	vm.Set("kGetegid",os.Getegid)
-	vm.Set("kArgs",os.Args)
+	vm.Set("kGetEnv", os.Getenv)
+	vm.Set("kSetEnv", os.Setenv)
+	vm.Set("kHostname", os.Hostname)
+	vm.Set("kGetpid", os.Getpid)
+	vm.Set("kGetppid", os.Getppid)
+	vm.Set("kGetgid", os.Getgid)
+	vm.Set("kGetuid", os.Getuid)
+	vm.Set("kGetegid", os.Getegid)
+	vm.Set("kArgs", os.Args)
 
-
-
-
+	vm.Set("kNow", time.Now)
 
 	vm.Run(code)
 
